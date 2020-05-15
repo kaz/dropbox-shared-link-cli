8bbd358bebc2c50c6d2c8c37177f5310923c4f1c
Kazuki Sawada <kazuki@6715.jp>
2020-05-15 20:03:41 +0900

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,53 @@
 mod dropbox;
 
-fn main() {
-    let client = dropbox::SharedLinkClient::new(
-        "https://www.dropbox.com/sh/arnpe0ef5wds8cv/AAAk_SECQ2Nc6SVGii3rHX6Fa/",
-    )
-    .expect("failed to setup client");
+use clap::Clap;
 
-    let lss = client.ls("/").expect("failed to run ls");
-    println!("{:?}", lss);
+#[derive(Clap, Debug)]
+struct Opts {
+    /// Root URL
+    #[clap(
+        short,
+        long,
+        default_value = "https://www.dropbox.com/sh/arnpe0ef5wds8cv/AAAk_SECQ2Nc6SVGii3rHX6Fa/"
+    )]
+    root: String,
+
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(clap::Clap, Debug)]
+enum SubCommand {
+    Ls(Ls),
+    Get(Get),
+}
+
+/// list files/directories
+#[derive(Clap, Debug)]
+struct Ls {
+    /// Where to ls (e.g. /ABC123/D )
+    path: String,
+}
+
+/// download file
+#[derive(Clap, Debug)]
+struct Get {
+    /// Which file to download (e.g. /ABC123/D/in/sample-1.in )
+    path: String,
+}
+
+fn main() {
+    let opts = Opts::parse();
+
+    let client = dropbox::SharedLinkClient::new(opts.root)
+        .expect("failed to setup client: invalid root URL");
+
+    match opts.subcmd {
+        SubCommand::Ls(Ls { path }) => {
+            for ent in client.ls(path).expect("ls failed") {
+                println!("{} {}", if ent.is_dir { "D" } else { "F" }, ent.filename)
+            }
+        }
+        SubCommand::Get(Get { path }) => unimplemented!(),
+    }
 }
